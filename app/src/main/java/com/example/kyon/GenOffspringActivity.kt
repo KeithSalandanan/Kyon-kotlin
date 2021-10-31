f2c866ff4ae262e1724ceb8fe5c362f94ca6eb5a
KeithSalandanan <keithsalandanan@gmail.com>
2021-10-31 22:58:44 +0800

    Added webView

--- app/src/main/java/com/example/kyon/GenOffspringActivity.kt
+++ app/src/main/java/com/example/kyon/GenOffspringActivity.kt
@@ -2,13 +2,29 @@ package com.example.kyon
 
 
 import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_gen_offspring.*
 
 class GenOffspringActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gen_offspring)
+
+        webView.webViewClient = object : WebViewClient(){
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+        }
+        //change the link
+        webView.loadUrl("https://www.google.com")
 
 
     }
